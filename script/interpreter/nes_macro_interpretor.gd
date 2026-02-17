ec1d590a6ebe6d2533cb0582274d44c00cfeb8a0
Eloi Stree <eloistree@gmail.com>
2026-02-17 09:25:28 +0100

    Commencer Interpeter avant de donner le pc a loelia

---
+++ script/interpreter/nes_macro_interpretor.gd
@@ -0,0 +1,34 @@
+class_name NesMacroInterpreter extends Node
+
+signal on_integer_to_send_requested(integer_action:int)
+signal on_integer_to_send_requested_with_milliseconds_delay(value_sent:int, milliseconds_delay:int)
+
+
+
+# func is_digital(text:char) -> bool:
+# 	match text:
+# 		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+# 			return true
+# 		_:
+# 			return false
+
+# func is_text_a_valid_integer(text:String) -> bool:
+# 	for c in text:
+# 		if not is_digital(c):
+# 			return false
+# 	return true
+
+
+
+func translate_text_to_integer_and_callable(text:String):
+	pass
+	
+	
+	# var words = text.split(" ",false)
+	# for w in words:
+	# 	if is_text_a_valid_integer(w):
+	# 		var integer_value = int(w)
+	# 		return integer_value
+	
+	
+	
